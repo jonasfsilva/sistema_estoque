ef56e7e034bd5d8ae968fa924cce4a52d8347a88
jonas <ferreirajonasss@gmail.com>
2017-11-19 12:01:05 -0200

    adicinoando database defalt

--- padaria/settings.py
+++ padaria/settings.py
@@ -85,6 +85,10 @@ DATABASES = {
 }
 
 
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
 
